c6d456e52ad4fa26d79c0fd705e278da8f5a626b
rodal <raulrodalsantome@gmail.com>
2024-02-26 22:00:51 +0100

    Añadido SQL de creacion de la BBDD

---
+++ contactos.sql
@@ -0,0 +1,31 @@
+-- Crear la base de datos
+CREATE DATABASE Agenda;
+GO
+
+-- Usar la base de datos creada
+USE Agenda;
+GO
+
+-- Crear la tabla de contactos
+CREATE TABLE Contactos (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Nombre VARCHAR(50),
+    Apellidos VARCHAR(50),
+    Comentario VARCHAR(MAX)
+);
+
+-- Crear la tabla de teléfonos
+CREATE TABLE Telefonos (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    ID_Contacto INT,
+    Telefono VARCHAR(15),
+    FOREIGN KEY (ID_Contacto) REFERENCES Contactos(ID)
+);
+
+-- Crear la tabla de emails
+CREATE TABLE Correos (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    ID_Contacto INT,
+    Correo VARCHAR(100),
+    FOREIGN KEY (ID_Contacto) REFERENCES Contactos(ID)
+);
