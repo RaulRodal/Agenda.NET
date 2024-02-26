8bc14bba8fc908b067d0fee447436d5f16a342c8
rodal <raulrodalsantome@gmail.com>
2024-02-26 21:58:17 +0100

    Agregar archivos de proyecto.

---
+++ Agenda/ConexionDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace Agenda
+{
+    internal class ConexionDB
+    {
+        private SqlConnection conexion;
+        private String database = "Agenda";
+        private String servidor = "localhost";
+        private String usuario = "sa";
+        private String password = "abc123.";
+        private String cadeaConexion;
+
+        public ConexionDB()
+        {
+            cadeaConexion = "Persist Security Info = False;" +
+                "; User id = " + usuario + ";" +
+                "; Password = " + password + ";" +
+                "Initial Catalog = " + database + ";" +
+                "Server = " + servidor + ";";
+        }
+
+        public SqlConnection getConexion()
+        {
+            if (conexion == null)
+            {
+                conexion = new SqlConnection(cadeaConexion);
+                conexion.Open();
+            }
+            return conexion;
+        } 
+
+    }
+}
---
+++ Agenda/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Agenda
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ConexionDB mConexion;
+        public MainWindow()
+        {
+            mConexion = new ConexionDB();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string sqlInsertContactos = "INSERT INTO dbo.Contactos (Nombre, Apellidos, Comentario) VALUES (@Nombre, @Apellidos, @Comentario)";
+            string sqlInsertTelefono = "INSERT INTO dbo.Telefonos (ID_Contacto, Telefono) VALUES (@ID_Contacto, @Telefono)";
+            string sqlInsertCorreo = "INSERT INTO dbo.Correos (ID_Contacto, Correo) VALUES (@ID_Contacto, @Correo)";
+
+            using (SqlCommand command = new SqlCommand(sqlInsertContactos, mConexion.getConexion()))
+            {
+                command.Parameters.AddWithValue("@Nombre", nombre.Text);
+                command.Parameters.AddWithValue("@Apellidos", apellidos.Text);
+                command.Parameters.AddWithValue("@Comentario", comentario.Text);
+
+                command.ExecuteNonQuery();
+            }
+
+            string sql = "SELECT MAX(Id) FROM dbo.Contactos";
+
+            using (SqlCommand commandID = new SqlCommand(sql, mConexion.getConexion()))
+            {
+                int ultimoId = (int)commandID.ExecuteScalar();
+
+                using (SqlCommand command = new SqlCommand(sqlInsertTelefono, mConexion.getConexion()))
+                {
+                    command.Parameters.AddWithValue("@ID_Contacto", ultimoId);
+                    command.Parameters.AddWithValue("@Telefono", telefono.Text);
+
+                    command.ExecuteNonQuery();
+                }
+
+                using (SqlCommand command = new SqlCommand(sqlInsertCorreo, mConexion.getConexion()))
+                {
+                    command.Parameters.AddWithValue("@ID_Contacto", ultimoId);
+                    command.Parameters.AddWithValue("@Correo", email.Text);
+
+                    command.ExecuteNonQuery();
+                }
+
+            }
+
+
+
+
+
+        }
+    }
+}
